9e94ff66482bfd5b3ed0f13268323d8c52366d83
Nick Smith <ncsmith6@wisc.edu>
2014-05-20 15:10:12 -0500

    Offline reconstruction comparison won't use endcap for now.

--- test/eff_hists_cfg.py
+++ test/eff_hists_cfg.py
@@ -73,7 +73,7 @@ process.reconstruction_step = cms.Path( process.calolocalreco )
 
 process.L1EGammaCrystalsProducer = cms.EDProducer("L1EGCrystalClusterProducerTest",
    DEBUG = cms.untracked.bool(False),
-   useECalEndcap = cms.untracked.bool(True)
+   useECalEndcap = cms.untracked.bool(False)
 )
 process.pSasha = cms.Path( process.L1EGammaCrystalsProducer )
 
@@ -115,7 +115,7 @@ process.analyzer = cms.EDAnalyzer('L1EGRateStudies',
 # use this when running over single particle gun sources
    doEfficiencyCalc = cms.untracked.bool(True),
    useOfflineClusters = cms.untracked.bool(True),
-   useEndcap = cms.untracked.bool(True),
+   useEndcap = cms.untracked.bool(False),
    hovere_cut_min = cms.untracked.double(1),
    hovere_cut_max = cms.untracked.double(4),
    ecal_isolation_cut_min = cms.untracked.double(1),
