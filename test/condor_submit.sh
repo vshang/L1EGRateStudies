fa0951b7b9ab42e1c5a62b1d2a399830ecf2e9ce
Nick Smith <ncsmith6@wisc.edu>
2014-05-30 15:04:38 -0500

    Modified submit scripts for published datasets.

--- test/condor_submit.sh
+++ test/condor_submit.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
-farmoutAnalysisJobs \
-   --input-dir=/store/user/ncsmith/L1TrackTriggerMC/ \
-   --input-files-per-job=1 \
-   --job-count=2 \
-   egalg_eff_hists $CMSSW_BASE eff_hists_cfg.py
+
+jobcount=''
+while getopts ":j:" opt; do
+  case $opt in
+    j)
+      jobcount="--job-count=$OPTARG"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+# remove old stuff
+rm /nfs_scratch/nsmith/egalg_* -r
+gsido rm /hdfs/store/user/nsmith/egalg_* -r
 
 farmoutAnalysisJobs \
-   --input-dir=root://eoscms.cern.ch/ \
-   --input-file-list=NeutrinoGunFiles.txt \
-   --assume-input-files-exist \
-   --input-files-per-job=10 \
-   --job-count=10 \
-   egalg_rate_hists $CMSSW_BASE rate_hists_cfg.py
+  --input-dir=/store/mc/TTI2023Upg14D/Neutrino_Pt2to20_gun/GEN-SIM-DIGI-RAW/PU140bx25_PH2_1K_FB_V3-v2/00000 \
+  --input-files-per-job=1 $jobcount \
+  egalg_eff_hists $CMSSW_BASE eff_hists_cfg.py
+
+farmoutAnalysisJobs \
+  --input-dir=/store/mc/TTI2023Upg14D/SingleElectronFlatPt0p2To50/GEN-SIM-DIGI-RAW/PU140bx25_PH2_1K_FB_V3-v2/00000 \
+  --input-files-per-job=1 $jobcount \
+  egalg_rate_hists $CMSSW_BASE rate_hists_cfg.py
