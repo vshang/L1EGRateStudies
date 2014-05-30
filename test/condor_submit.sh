56836567e6a7ebbcf0966a65c7710aaa5d4a0c66
Nick Smith <ncsmith6@wisc.edu>
2014-05-30 15:59:42 -0500

    Bugfix in condor submission!

--- test/condor_submit.sh
+++ test/condor_submit.sh
@@ -22,11 +22,11 @@ rm /nfs_scratch/nsmith/egalg_* -r
 gsido rm /hdfs/store/user/nsmith/egalg_* -r
 
 farmoutAnalysisJobs \
-  --input-dir=/store/mc/TTI2023Upg14D/Neutrino_Pt2to20_gun/GEN-SIM-DIGI-RAW/PU140bx25_PH2_1K_FB_V3-v2/00000 \
+  --input-dir=/store/mc/TTI2023Upg14D/SingleElectronFlatPt0p2To50/GEN-SIM-DIGI-RAW/PU140bx25_PH2_1K_FB_V3-v2/00000 \
   --input-files-per-job=1 $jobcount \
   egalg_eff_hists $CMSSW_BASE eff_hists_cfg.py
 
 farmoutAnalysisJobs \
-  --input-dir=/store/mc/TTI2023Upg14D/SingleElectronFlatPt0p2To50/GEN-SIM-DIGI-RAW/PU140bx25_PH2_1K_FB_V3-v2/00000 \
+  --input-dir=/store/mc/TTI2023Upg14D/Neutrino_Pt2to20_gun/GEN-SIM-DIGI-RAW/PU140bx25_PH2_1K_FB_V3-v2/00000 \
   --input-files-per-job=1 $jobcount \
   egalg_rate_hists $CMSSW_BASE rate_hists_cfg.py
