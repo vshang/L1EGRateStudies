985dee996dda1444b1c98a2541ad578d6715b8ee
Tyler Ruggles <truggles@wisc.edu>
2016-05-10 16:53:23 +0200

    Working towards non-rootools based, python plotting

---
+++ test/normalizer.py
@@ -0,0 +1,65 @@
+import ROOT
+import trigHelpers
+
+def normalizeHists() :
+    eff = ROOT.TFile("egTriggerEff.root", "UPDATE")
+    rates = ROOT.TFile("egTriggerRates.root", "UPDATE")
+
+    # We need to renormalize everything since these files were parallel processed
+ 
+    effHistKeys = trigHelpers.getKeysOfClass(eff, "analyzer", "TH1F")
+    effPtDenom = eff.Get("analyzer/gen_pt")
+    effEtaDenom = eff.Get("analyzer/gen_eta")
+    effRecoPtDenom = eff.Get("analyzer/reco_pt")
+    if effPtDenom != None :
+        print "Dividing efficiency histograms by gen/reco hists"
+        print "Total event count: %f" % effPtDenom.Integral()
+        print "Total offline reco event count: %f" % effRecoPtDenom.Integral()
+
+       
+        eff.cd("analyzer") 
+        effPtHists = trigHelpers.loadObjectsMatchingPattern( eff, "analyzer", effHistKeys, "*_efficiency*pt" )
+        for hist in effPtHists :
+            if "reco_" not in hist.GetName() :
+                denom = effPtDenom.Clone()
+            if "reco_" in hist.GetName() :
+                denom = effRecoPtDenom.Clone()
+            print "    Dividing " + hist.GetName() + " by " + denom.GetName()
+            graph = ROOT.TGraphAsymmErrors(hist, denom)
+            graph.GetXaxis().SetTitle(hist.GetXaxis().GetTitle())
+            graph.GetYaxis().SetTitle("Efficiency")
+            graph.Write()
+
+
+        effEtaHists = trigHelpers.loadObjectsMatchingPattern(eff, "analyzer", effHistKeys, "*_efficiency*eta")
+        effEtaDenom.Sumw2()
+        for hist in effEtaHists :
+            graph = ROOT.TGraphAsymmErrors(hist, effEtaDenom)
+            graph.GetXaxis().SetTitle(hist.GetXaxis().GetTitle())
+            graph.GetYaxis().SetTitle("Efficiency")
+            graph.Write()
+
+        dir_ = eff.Get("analyzer")
+        #dir_.Delete("gen_pt*")
+        #dir_.Delete("gen_eta*")
+        eff.Write("", ROOT.TObject.kOverwrite)
+
+
+    rateHistKeys = trigHelpers.getKeysOfClass(rates, "analyzer", "TH1F")
+    if rates.Get("analyzer/eventCount") != None :
+        print "Normalizing rate histograms to 30MHz"
+        nEvents = rates.Get("analyzer/eventCount").GetBinContent(1)
+        print "Total event count:", nEvents
+        rateHists = trigHelpers.loadObjectsMatchingPattern(rates, "analyzer", rateHistKeys, "*_rate*")
+        dir_ = rates.Get("analyzer")
+        dir_.cd()
+        for hist in rateHists :
+            hist.Sumw2()
+            hist.Scale(30000./nEvents)
+        #dir_.Delete("eventCount*")
+        rates.Write("", ROOT.TObject.kOverwrite)
+    
+
+
+if __name__ == '__main__' :
+    normalizeHists()
---
+++ test/trigHelpers.py
@@ -0,0 +1,44 @@
+import ROOT
+
+
+def getKeysOfClass( file_, dir_, class_ ) :
+    keys = []
+    d = file_.Get( dir_ )
+    allKeys = d.GetListOfKeys()
+
+    #print "keys of class"
+    for k in allKeys :
+        if k.GetClassName() == class_ :
+            keys.append( k )
+
+    return keys
+
+
+def loadObjectsMatchingPattern( file_, dir_, keys, matchString ) :
+    hists = []
+    parts = matchString.split('*')
+    for p in parts : print p
+    for key in keys :
+        allSuccess = True
+        for part in parts :
+            if not part in key.GetName() :
+                allSuccess = False
+        if allSuccess :
+            #print "append"
+            hists.append( file_.Get( dir_ + '/' + key.GetName() ) )
+            #hists.append( file_.GetObject( key.GetName(), key.GetClassName() ) )
+            #print hists[-1]
+    return hists
+
+
+
+if __name__ == '__main__' :
+    f = ROOT.TFile('egTriggerEff.root','r')
+    dir_ = 'analyzer'
+    keys = getKeysOfClass( f, dir_, 'TH1F' )
+    for key in keys :
+        print key, key.GetName(), key.GetClassName()
+
+    hists = loadObjectsMatchingPattern( f, dir_, keys, "*_efficiency*_pt" )
+    for h in hists :
+        print h
