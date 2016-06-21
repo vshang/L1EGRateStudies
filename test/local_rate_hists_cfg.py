e9c98456e8460936a7edff6180a3d4de43e1639f
Tyler Ruggles <truggles@wisc.edu>
2016-06-21 02:26:28 -0500

    Files to test new configurations and additions locally, run with cmsRun

---
+++ test/local_rate_hists_cfg.py
@@ -0,0 +1,130 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("test")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.categories = cms.untracked.vstring('L1EGRateStudies', 'FwkReport')
+process.MessageLogger.cerr.FwkReport = cms.untracked.PSet(
+   reportEvery = cms.untracked.int32(1)
+)
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(20) )
+
+process.source = cms.Source("PoolSource",
+   fileNames = cms.untracked.vstring('file:/hdfs/store/mc/TTI2023Upg14D/SingleElectronFlatPt0p2To50/GEN-SIM-DIGI-RAW/PU140bx25_PH2_1K_FB_V3-v2/00000/80EE54BB-1EE6-E311-877F-002354EF3BE0.root')
+)
+
+# All this stuff just runs the various EG algorithms that we are studying
+                         
+# ---- Global Tag :
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'PH2_1K_FB_V3::All', '')
+
+process.load('Configuration.Geometry.GeometryExtended2023TTIReco_cff')
+
+process.load('Configuration/StandardSequences/L1HwVal_cff')
+process.load('Configuration.StandardSequences.RawToDigi_cff')
+process.load("SLHCUpgradeSimulations.L1CaloTrigger.SLHCCaloTrigger_cff")
+
+# bug fix for missing HCAL TPs in MC RAW
+from SimCalorimetry.HcalTrigPrimProducers.hcaltpdigi_cff import HcalTPGCoderULUT
+HcalTPGCoderULUT.LUTGenerationMode = cms.bool(True)
+process.valRctDigis.hcalDigis             = cms.VInputTag(cms.InputTag('valHcalTriggerPrimitiveDigis'))
+process.L1CaloTowerProducer.HCALDigis =  cms.InputTag("valHcalTriggerPrimitiveDigis")
+
+process.slhccalo = cms.Path( process.RawToDigi + process.valHcalTriggerPrimitiveDigis+process.SLHCCaloTrigger)
+
+# run L1Reco to produce the L1EG objects corresponding
+# to the current trigger
+process.load('Configuration.StandardSequences.L1Reco_cff')
+process.L1Reco = cms.Path( process.l1extraParticles )
+
+# producer for UCT2015 / Stage-1 trigger objects
+process.load("L1Trigger.UCT2015.emulationMC_cfi")
+process.load("L1Trigger.UCT2015.uctl1extraparticles_cfi")
+process.pUCT = cms.Path(
+    process.emulationSequence *
+    process.uct2015L1Extra
+)
+
+# --------------------------------------------------------------------------------------------
+#
+# ----    Produce the L1EGCrystal clusters (code of Sasha Savin)
+
+# first you need the ECAL RecHIts :
+process.load('Configuration.StandardSequences.Reconstruction_cff')
+process.reconstruction_step = cms.Path( process.calolocalreco )
+
+process.L1EGammaCrystalsProducer = cms.EDProducer("L1EGCrystalClusterProducer",
+   EtminForStore = cms.double(0.),
+   debug = cms.untracked.bool(False),
+   useECalEndcap = cms.bool(True)
+)
+process.pSasha = cms.Path( process.L1EGammaCrystalsProducer )
+
+# --------------------------------------------------------------------------------------------
+#
+# ----  Match the L1EG stage-2 objects created by the SLHCCaloTrigger sequence above
+#	with the crystal-level clusters.
+#	This produces a new collection of L1EG objects, starting from the original
+#	L1EG collection. The eta and phi of the L1EG objects is corrected using the
+#	information of the xtal level clusters.
+
+process.l1ExtraCrystalProducer = cms.EDProducer("L1ExtraCrystalPosition",
+   eGammaSrc = cms.InputTag("SLHCL1ExtraParticles","EGamma"),
+   eClusterSrc = cms.InputTag("L1EGammaCrystalsProducer","EGCrystalCluster")
+)
+process.egcrystal_producer = cms.Path(process.l1ExtraCrystalProducer)
+
+# ---------------------------------------------------------------------------
+#
+# --- Create the collection of special tracks for electrons
+#
+
+process.load("SLHCUpgradeSimulations.L1TrackTrigger.L1TrackingSequence_cfi")
+process.pTracking = cms.Path( process.ElectronTrackingSequence )
+
+
+# --- Produce the L1TkPrimaryVertex
+
+process.load("SLHCUpgradeSimulations.L1TrackTrigger.L1TkPrimaryVertexProducer_cfi")
+process.pL1TkPrimaryVertex = cms.Path( process.L1TkPrimaryVertex )
+
+
+
+# ----------------------------------------------------------------------------------------------
+# 
+# Analyzer starts here
+
+process.analyzer = cms.EDAnalyzer('L1EGRateStudies',
+   L1EGammaInputTags = cms.VInputTag(
+      # Old stage-2 trigger
+      cms.InputTag("SLHCL1ExtraParticles","EGamma"),
+      # 'dynamic clustering'
+      cms.InputTag("SLHCL1ExtraParticlesNewClustering","EGamma"),
+      # Run 1 algo.
+      cms.InputTag("l1extraParticles", "Isolated"),
+      cms.InputTag("l1extraParticles", "NonIsolated"),
+      # UCT alg.
+      cms.InputTag("l1extraParticlesUCT", "Isolated"),
+      cms.InputTag("l1extraParticlesUCT", "NonIsolated"),
+      # Crystal-level algo.
+      cms.InputTag("L1EGammaCrystalsProducer","EGammaCrystal")
+   ),
+   L1CrystalClustersInputTag = cms.InputTag("L1EGammaCrystalsProducer","EGCrystalCluster"),
+   L1TrackInputTag = cms.InputTag("TTTracksFromPixelDigisLargerPhi","Level1TTTracks"),
+   L1TrackPrimaryVertexTag = cms.InputTag("L1TkPrimaryVertex"),
+   doEfficiencyCalc = cms.untracked.bool(False),
+   useEndcap = cms.untracked.bool(False),
+   histogramBinCount = cms.untracked.int32(40),
+   histogramRangeLow = cms.untracked.double(0),
+   histogramRangeHigh = cms.untracked.double(50)
+)
+
+process.panalyzer = cms.Path(process.analyzer)
+
+process.TFileService = cms.Service("TFileService", 
+   fileName = cms.string("tmp_rate.root"), 
+   closeFileFast = cms.untracked.bool(True)
+)
