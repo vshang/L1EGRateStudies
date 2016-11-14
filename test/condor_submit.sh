a1b5bbf887d3c4ed140d06e63d8b402066d8f76d
Tyler Ruggles <truggles@wisc.edu>
2016-11-14 11:59:28 -0600

    Added photo sample to study

--- test/condor_submit.sh
+++ test/condor_submit.sh
@@ -5,7 +5,7 @@
 #scram b
 #popd
 
-jobNum=26
+jobNum=48
 jobopts=''
 doeff=true
 dorate=true
@@ -43,6 +43,7 @@ while getopts ":trefj:" opt; do
 done
 
 if $doeff; then
+    # Single Electrong
     if $rmold; then
         rm /nfs_scratch/${USER}/egalg_eff* -r
         gsido rm /hdfs/store/user/${USER}/egalg_eff* -r
@@ -51,6 +52,15 @@ if $doeff; then
         --input-dir=/store/mc/TTI2023Upg14D/SingleElectronFlatPt0p2To50/GEN-SIM-DIGI-RAW/PU140bx25_PH2_1K_FB_V3-v2/00000 \
         --input-files-per-job=1 $jobopts \
         egalg_eff_hists${jobNum} $CMSSW_BASE eff_hists_cfg.py
+    # Single Photo
+    if $rmold; then
+        rm /nfs_scratch/${USER}/egalgPho_eff* -r
+        gsido rm /hdfs/store/user/${USER}/egalgPho_eff* -r
+    fi
+    farmoutAnalysisJobs \
+        --input-dir=/store/mc/TTI2023Upg14D/SinglePhotonFlatPt5To75/GEN-SIM-DIGI-RAW/PU140bx25_PH2_1K_FB_V3-v2/00000 \
+        --input-files-per-job=1 $jobopts \
+        egalgPho_eff_hists${jobNum} $CMSSW_BASE eff_hists_cfg.py
 fi
 
 if $dorate; then
--- test/normalizer.py
+++ test/normalizer.py
@@ -3,47 +3,50 @@ import trigHelpers
 
 def normalizeHists() :
     eff = ROOT.TFile("egTriggerEff.root", "UPDATE")
+    pho = ROOT.TFile("egTriggerPhoEff.root", "UPDATE")
     rates = ROOT.TFile("egTriggerRates.root", "UPDATE")
 
     # We need to renormalize everything since these files were parallel processed
- 
-    effHistKeys = trigHelpers.getKeysOfClass(eff, "analyzer", "TH1F")
-    effPtDenom = eff.Get("analyzer/gen_pt")
-    effEtaDenom = eff.Get("analyzer/gen_eta")
-    effRecoPtDenom = eff.Get("analyzer/reco_pt")
-    if effPtDenom != None :
-        print "Creating efficiency histograms"
-	print "Eff hists in 2 categories 1) gen / gen and 2) reco / reco"
-        print "Total event count: %f" % effPtDenom.Integral()
-        print "Total offline reco event count: %f" % effRecoPtDenom.Integral()
 
-       
-        eff.cd("analyzer") 
-        effPtHists = trigHelpers.loadObjectsMatchingPattern( eff, "analyzer", effHistKeys, "*_efficiency*pt" )
-        for hist in effPtHists :
-            if "reco_" not in hist.GetName() :
-                denom = effPtDenom.Clone()
-            if "reco_" in hist.GetName() :
-                denom = effRecoPtDenom.Clone()
-            print "    Dividing " + hist.GetName() + " by " + denom.GetName()
-            graph = ROOT.TGraphAsymmErrors(hist, denom)
-            graph.GetXaxis().SetTitle(hist.GetXaxis().GetTitle())
-            graph.GetYaxis().SetTitle("Efficiency")
-            graph.Write()
+    # Single Electrong and Photon are idential 
+    for file in [eff, pho] :
+        effHistKeys = trigHelpers.getKeysOfClass(file, "analyzer", "TH1F")
+        effPtDenom = eff.Get("analyzer/gen_pt")
+        effEtaDenom = eff.Get("analyzer/gen_eta")
+        effRecoPtDenom = eff.Get("analyzer/reco_pt")
+        if effPtDenom != None :
+            print "Creating efficiency histograms"
+	    print "Eff hists in 2 categories 1) gen / gen and 2) reco / reco"
+            print "Total event count: %f" % effPtDenom.Integral()
+            print "Total offline reco event count: %f" % effRecoPtDenom.Integral()
+
+           
+            file.cd("analyzer") 
+            effPtHists = trigHelpers.loadObjectsMatchingPattern( file, "analyzer", effHistKeys, "*_efficiency*pt" )
+            for hist in effPtHists :
+                if "reco_" not in hist.GetName() :
+                    denom = effPtDenom.Clone()
+                if "reco_" in hist.GetName() :
+                    denom = effRecoPtDenom.Clone()
+                print "    Dividing " + hist.GetName() + " by " + denom.GetName()
+                graph = ROOT.TGraphAsymmErrors(hist, denom)
+                graph.GetXaxis().SetTitle(hist.GetXaxis().GetTitle())
+                graph.GetYaxis().SetTitle("Efficiency")
+                graph.Write()
 
 
-        effEtaHists = trigHelpers.loadObjectsMatchingPattern(eff, "analyzer", effHistKeys, "*_efficiency*eta")
-        effEtaDenom.Sumw2()
-        for hist in effEtaHists :
-            graph = ROOT.TGraphAsymmErrors(hist, effEtaDenom)
-            graph.GetXaxis().SetTitle(hist.GetXaxis().GetTitle())
-            graph.GetYaxis().SetTitle("Efficiency")
-            graph.Write()
+            effEtaHists = trigHelpers.loadObjectsMatchingPattern(file, "analyzer", effHistKeys, "*_efficiency*eta")
+            effEtaDenom.Sumw2()
+            for hist in effEtaHists :
+                graph = ROOT.TGraphAsymmErrors(hist, effEtaDenom)
+                graph.GetXaxis().SetTitle(hist.GetXaxis().GetTitle())
+                graph.GetYaxis().SetTitle("Efficiency")
+                graph.Write()
 
-        dir_ = eff.Get("analyzer")
-        #dir_.Delete("gen_pt*")
-        #dir_.Delete("gen_eta*")
-        eff.Write("", ROOT.TObject.kOverwrite)
+            dir_ = file.Get("analyzer")
+            #dir_.Delete("gen_pt*")
+            #dir_.Delete("gen_eta*")
+            file.Write("", ROOT.TObject.kOverwrite)
 
 
     rateHistKeys = trigHelpers.getKeysOfClass(rates, "analyzer", "TH1F")
@@ -65,14 +68,6 @@ def normalizeHists() :
     # Add a variable to scale L1EG cluster pt to
     # RCT 2015 pt 
     print "Adding crystal_pt_to_RCT2015 to crystal_tree"
-    from array import array
-    effTree = eff.Get('analyzer/crystal_tree')
-    rateTree = rates.Get('analyzer/crystal_tree')
-    rateCorPt = array('f', [ 0 ] )
-    rateCorPtB = rateTree.Branch('crystal_pt_to_RCT2015', rateCorPt, 'crystal_pt_to_RCT2015/F')
-    effCorPt = array('f', [ 0 ] )
-    effCorPtB = effTree.Branch('crystal_pt_to_RCT2015', effCorPt, 'crystal_pt_to_RCT2015/F')
-
     # For fit parameter values, run "drawPointsHists" in plotCutThresholds.py
     corFunc = ROOT.TF1('f1', '(-([0] + [1]*TMath::Exp(-[2]*x))+([3] + [4]*TMath::Exp(-[5]*x)))')
     corFunc.SetParameter( 0, -0.041252 )
@@ -81,6 +76,29 @@ def normalizeHists() :
     corFunc.SetParameter( 3, 0.113996 )
     corFunc.SetParameter( 4, -0.268292 )
     corFunc.SetParameter( 5, 0.072791 )
+
+    from array import array
+    for file in [eff, pho] :
+        effTree = file.Get('analyzer/crystal_tree')
+        effCorPt = array('f', [ 0 ] )
+        effCorPtB = effTree.Branch('crystal_pt_to_RCT2015', effCorPt, 'crystal_pt_to_RCT2015/F')
+
+        cnt = 0
+        for i in range(0, effTree.GetEntries() ) :
+            effTree.GetEntry( i )
+            cnt += 1
+            if cnt % 1000 == 0 : print "Efficiency Tree: %i" % cnt
+	    pt = effTree.cluster_pt
+            effCorPt[0] = pt + corFunc( pt )*pt
+            effCorPtB.Fill()
+        file.Write("", ROOT.TObject.kOverwrite)
+        file.Close()
+
+
+
+    rateTree = rates.Get('analyzer/crystal_tree')
+    rateCorPt = array('f', [ 0 ] )
+    rateCorPtB = rateTree.Branch('crystal_pt_to_RCT2015', rateCorPt, 'crystal_pt_to_RCT2015/F')
 
     cnt = 0
     for i in range(0, rateTree.GetEntries() ) :
@@ -92,17 +110,6 @@ def normalizeHists() :
         rateCorPtB.Fill()
     rates.Write("", ROOT.TObject.kOverwrite)
     rates.Close() 
-
-    cnt = 0
-    for i in range(0, effTree.GetEntries() ) :
-        effTree.GetEntry( i )
-        cnt += 1
-        if cnt % 1000 == 0 : print "Efficiency Tree: %i" % cnt
-	pt = effTree.cluster_pt
-        effCorPt[0] = pt + corFunc( pt )*pt
-        effCorPtB.Fill()
-    eff.Write("", ROOT.TObject.kOverwrite)
-    eff.Close()
 
 
 
