d62a78803ba5c83d9335c3088dbcb17573f68afc
Tyler Ruggles <truggles@wisc.edu>
2017-07-19 17:35:39 +0200

    Cleaner for normalizing more files

--- test/normalizer.py
+++ test/normalizer.py
@@ -4,15 +4,29 @@ import trigHelpers
 version = 'v9'
 
 def normalizeHists() :
-    eff = ROOT.TFile("r2_phase2_singleElectron_%s.root" % version, "UPDATE")
-    pho = ROOT.TFile("r2_phase2_singlePhoton_%s.root" % version, "UPDATE")
-    piZero = ROOT.TFile("r2_phase2_singlePiZero_%s.root" % version, "UPDATE")
-    rates = ROOT.TFile("r2_phase2_minBias_%s.root" % version, "UPDATE")
+    #eff = ROOT.TFile("r2_phase2_singleElectron_%s.root" % version, "UPDATE")
+    #pho = ROOT.TFile("r2_phase2_singlePhoton_%s.root" % version, "UPDATE")
+    #piZero = ROOT.TFile("r2_phase2_singlePiZero_%s.root" % version, "UPDATE")
+    #rates = ROOT.TFile("r2_phase2_minBias_%s.root" % version, "UPDATE")
+    effFiles = []
+        'r2_phase2_singleElectron_top20.root',
+        'r2_phase2_singleElectron_20170717noSkimRecoPerCard36v2.root',
+        'r2_phase2_singleElectron_20170716top05.root',
+        'r2_phase2_singleElectron_20170716top10.root',
+        'r2_phase2_singleElectron_20170716top20.root',
+        'r2_phase2_singleElectron_20170612v1.root',]
+    rateFiles = [
+        'r2_phase2_minBias_20170716top10.root',
+        'r2_phase2_minBias_20170716top05.root',
+        'r2_phase2_minBias_20170716top20.root',
+        'r2_phase2_minBias_20170717noSkimRecoPerCard36v2.root',
+        'r2_phase2_minBias_20170612v1.root']
 
     # We need to renormalize everything since these files were parallel processed
 
     # Single Electron and Photon are idential 
-    for file in [eff, pho, piZero, ] :
+    for fName in effFiles :
+        file = ROOT.TFile( fName,  "UPDATE")
         effHistKeys = trigHelpers.getKeysOfClass(file, "analyzer", "TH1F")
         effPtDenom = file.Get("analyzer/gen_pt")
         effEtaDenom = file.Get("analyzer/gen_eta")
@@ -53,23 +67,27 @@ def normalizeHists() :
             #dir_.Delete("gen_pt*")
             #dir_.Delete("gen_eta*")
             file.Write("", ROOT.TObject.kOverwrite)
+        file.Close()
 
 
-    doRate = False
+    doRate = True
     if doRate :
-        rateHistKeys = trigHelpers.getKeysOfClass(rates, "analyzer", "TH1F")
-        if rates.Get("analyzer/eventCount") != None :
-            print "Normalizing rate histograms to 30MHz"
-            nEvents = rates.Get("analyzer/eventCount").GetBinContent(1)
-            print "Total event count:", nEvents
-            rateHists = trigHelpers.loadObjectsMatchingPattern(rates, "analyzer", rateHistKeys, "*_rate*")
-            dir_ = rates.Get("analyzer")
-            dir_.cd()
-            for hist in rateHists :
-                hist.Sumw2()
-                hist.Scale(30000./nEvents)
-            #dir_.Delete("eventCount*")
-            rates.Write("", ROOT.TObject.kOverwrite)
+        for fName in rateFiles :
+            file = ROOT.TFile( fName,  "UPDATE")
+            rateHistKeys = trigHelpers.getKeysOfClass(file, "analyzer", "TH1F")
+            if file.Get("analyzer/eventCount") != None :
+                print "Normalizing rate histograms to 30MHz"
+                nEvents = file.Get("analyzer/eventCount").GetBinContent(1)
+                print "Total event count:", nEvents
+                rateHists = trigHelpers.loadObjectsMatchingPattern(file, "analyzer", rateHistKeys, "*_rate*")
+                dir_ = file.Get("analyzer")
+                dir_.cd()
+                for hist in rateHists :
+                    hist.Sumw2()
+                    hist.Scale(30000./nEvents)
+                #dir_.Delete("eventCount*")
+                file.Write("", ROOT.TObject.kOverwrite)
+            file.Close()
 
 
 
