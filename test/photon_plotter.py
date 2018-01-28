6dc5bad6a2d1f77439274d7d457b47f11adbd77c
Tyler Ruggles <truggles@wisc.edu>
2018-01-28 22:05:56 +0100

    Some eta dependent pt resolution checks

--- test/photon_plotter.py
+++ test/photon_plotter.py
@@ -30,7 +30,7 @@ def setLegStyle( x1,y1,x2,y2 ) :
 
 #etList = ['minEt0MeV', 'minEt50MeV', 'minEt125MeV', 'minEt250MeV', 'minEt375MeV', 'minEt500MeV']
 etMap = OrderedDict()
-etMap[ 'minEt0MeV'   ] = 'ET > 0 MeV'
+#etMap[ 'minEt0MeV'   ] = 'ET > 0 MeV'
 etMap[ 'minEt125MeV' ] = 'ET >= 125 MeV'
 etMap[ 'minEt250MeV' ] = 'ET >= 250 MeV'
 etMap[ 'minEt375MeV' ] = 'ET >= 375 MeV'
@@ -39,7 +39,10 @@ etMap[ 'minEt500MeV' ] = 'ET >= 500 MeV'
 
 
 def getPtRes( name, cut = '' ) :
-    f = ROOT.TFile( 'newPhotonRes_'+name+'.root', 'r' )
+    iDir = '/afs/cern.ch/work/t/truggles/TrackTrigger/CMSSW_9_2_0/src/L1Trigger/L1EGRateStudies/test/'
+    fNameBase = 'r2_phase2_singleElectron_20180120'
+    f = ROOT.TFile( iDir+fNameBase+name+'.root', 'r' )
+    print f
     t = f.Get('analyzer/crystal_tree')
     h = ROOT.TH1D( name, name, 100, -.3, .1 )
     t.Draw( '(cluster_pt-gen_pt)/gen_pt >> '+name, cut )
@@ -48,10 +51,13 @@ def getPtRes( name, cut = '' ) :
     return h
 
 def plotRes( c, hists, name ) :
-    universalSaveDir = "/afs/cern.ch/user/t/truggles/www/Phase-II/photonPtRes/"
+    universalSaveDir = "/afs/cern.ch/user/t/truggles/www/Phase-II/photonPtRes3/"
     c.Clear()
-    if 'Zero' in name :
-        ROOT.gPad.SetLeftMargin( ROOT.gPad.GetLeftMargin() * 1.25 )
+    #if 'Zero' in name :
+    #    #ROOT.gPad.SetLeftMargin( ROOT.gPad.GetLeftMargin() * 1.25 )
+    #    ROOT.gPad.SetLeftMargin( ROOT.gPad.GetLeftMargin() * 1.25 )
+    #ROOT.gPad.SetLeftMargin( ROOT.gPad.GetLeftMargin() * 1.3 )
+    ROOT.gPad.SetLeftMargin( .13 )
     cnt = 0
     colors = [ROOT.kRed, ROOT.kBlue, ROOT.kMagenta, ROOT.kBlack, ROOT.kGreen+1]
 
@@ -77,11 +83,12 @@ def plotRes( c, hists, name ) :
         cnt += 1
 
     hists[0].SetMaximum( maxi * 1.2 )
-    leg = setLegStyle(0.2,0.4,0.5,0.8)
+    leg = setLegStyle(0.15,0.5,0.45,0.87)
     for h in hists :
-        leg.AddEntry(h, etMap[ h.GetTitle().split('_')[1] ],"lpe")
+        leg.AddEntry(h, etMap[ h.GetTitle().split('_')[-1] ],"lpe")
     leg.Draw("same")
-    hists[0].SetTitle( '%s L1EG p_{T} Resolution' % (hists[0].GetTitle().split('_')[0]) )
+    #hists[0].SetTitle( '%s L1EG p_{T} Resolution' % (hists[0].GetTitle().split('_')[0]) )
+    hists[0].SetTitle( 'Electron L1EG p_{T} Resolution' )
     c.Update()
     c.SaveAs( universalSaveDir+'ptRes_'+name+'.png' )
 
@@ -93,16 +100,35 @@ p.Draw()
 p.cd()
 
 pu200Hists = []
+pu200HistsCut = []
+pu200HistsCutEta = []
+pu200HistsCutEtaGtr1p4 = []
+pu200HistsCutEtaIn = []
+pu200HistsCutEtaOut = []
 puZeroHists = []
 
 for et in etMap.keys() :
 
-    puZeroHists.append( getPtRes( 'PUZero_'+et ) )
-    pu200Hists.append( getPtRes( 'PU200_'+et ) )
+    #puZeroHists.append( getPtRes( 'PUZero_'+et ) )
+    #pu200Hists.append( getPtRes( 'PU200_'+et ) )
+
+    pu200Hists.append( getPtRes( et ) )
+    pu200HistsCut.append( getPtRes( et, 'cluster_pt > 30 && cluster_pt < 40' ) )
+    pu200HistsCutEta.append( getPtRes( et, 'cluster_pt > 30 && cluster_pt < 40 && abs( eta ) < 1.4' ) )
+    pu200HistsCutEtaGtr1p4.append( getPtRes( et, 'cluster_pt > 30 && cluster_pt < 40 && abs( eta ) > 1.4' ) )
+    pu200HistsCutEtaIn.append( getPtRes( et, 'cluster_pt > 30 && cluster_pt < 40 && abs( eta ) < 1' ) )
+    pu200HistsCutEtaOut.append( getPtRes( et, 'cluster_pt > 30 && cluster_pt < 40 && abs( eta ) > 1' ) )
 
 
-plotRes( c, puZeroHists, 'PUZero' )
-plotRes( c, pu200Hists, 'PU200' )
+#plotRes( c, puZeroHists, 'PUZero' )
+#plotRes( c, pu200Hists, 'PU200' )
+
+#plotRes( c, pu200Hists, 'PU200_Electron' )
+plotRes( c, pu200HistsCut, 'PU200_Electron_pt30-40' )
+plotRes( c, pu200HistsCutEta, 'PU200_Electron_pt30-40_Eta1p4' )
+plotRes( c, pu200HistsCutEtaGtr1p4, 'PU200_Electron_pt30-40_EtaGtr1p4' )
+plotRes( c, pu200HistsCutEtaIn, 'PU200_Electron_pt30-40_Eta1' )
+plotRes( c, pu200HistsCutEtaOut, 'PU200_Electron_pt30-40_EtaGtr1' )
 
 
 
