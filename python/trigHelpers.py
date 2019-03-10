51981876a9476bedc5a34a05214d1ec0acd402bc
Tyler Ruggles <truggles@wisc.edu>
2019-03-10 15:35:48 +0100

    Easy round 2 file loading

--- python/trigHelpers.py
+++ python/trigHelpers.py
@@ -264,6 +264,23 @@ def drawDRHists(hists, c, ymax, plotDir, doFit = False, skipScale = False ) :
 
     c.Clear()
 
+# For processing samples in two rounds, this loads edm files from round1 into
+# the round 2 cfg
+def getSampleFiles( name ) :
+    import FWCore.ParameterSet.Config as cms
+
+    fileNames = cms.untracked.vstring()
+    print os.getenv('CMSSW_BASE')+'/src/L1Trigger/L1EGRateStudies/test/Round2Files/'+name+'.txt'
+    file = open(os.getenv('CMSSW_BASE')+'/src/L1Trigger/L1EGRateStudies/test/Round2Files/'+name+'.txt')
+    for line in file :
+        line = line.strip()
+        fileNames.append( 'file:'+line )    
+    print "\n\nTarget Files:",name
+    print "Loaded Files:"
+    print fileNames
+    print "\n\n"
+    return fileNames
+
 
 
 
